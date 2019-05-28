749c48535fcf0e25e9fa3acb69511cb4f984d00b
vigism <rushilvig@gmail.com>
2019-05-28 15:43:54 -0700

    Create Question, User, Reply, QuestionLike, QuestionFollow classes

--- sql_aa_questions/questions.rb
+++ sql_aa_questions/questions.rb
@@ -11,3 +11,235 @@ class QuestionsDatabase < SQLite3::Database
   end
 end
 
+
+class User
+  attr_accessor :id, :fname, :lname
+  
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+    data.map { |datum| User.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        id = ?
+    SQL
+
+    User.new(data.first)
+  end
+
+  def self.find_by_name(fname, lname)
+    data = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        fname = ? AND lname = ?
+      
+      
+    SQL
+
+    data.map { |datum| User.new(datum) }
+  end
+  
+  def initialize(options)
+    @id = options['id']    
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
+  def authored_questions()
+    Question.find_by_author_id(id)
+  end
+
+  def authored_replies()
+    Reply.find_by_user_id(id)
+  end
+
+end
+
+class Question
+  attr_accessor :id, :title, :body, :ass_author
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
+    data.map { |datum| Question.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        id = ?
+    SQL
+
+    Question.new(data.first)
+  end
+
+  def self.find_by_author_id(author_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        ass_author = ?
+    SQL
+
+    data.map { |datum| Question.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @ass_author = options['ass_author']
+  end
+
+  def author
+    User.find_by_id(ass_author)
+  end
+
+  def replies
+    Reply::find_by_question_id(id)
+  end
+end
+
+class QuestionFollow
+  attr_accessor :id, :user_id, :question_id
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM question_follows")
+    data.map { |datum| QuestionFollow.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        question_follows
+      WHERE
+        id = ?
+    SQL
+
+    QuestionFollow.new(data.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end
+end
+
+class Reply
+  attr_accessor :id, :question_id, :parent_reply_id, :user_id, :body, :subject
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM replies")
+    data.map { |datum| Reply.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        id = ?
+    SQL
+
+    Reply.new(data.first)
+  end
+
+   def self.find_by_user_id(user_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        user_id = ?
+    SQL
+
+    data.map { |datum| Reply.new(datum) }
+  end
+
+  def self.find_by_question_id(question_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        question_id = ?
+    SQL
+
+    data.map { |datum| Reply.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @parent_reply_id = options['parent_reply_id']
+    @user_id = options['user_id']
+    @body = options['body']
+    @subject = options['subject']
+  end
+
+  def author
+    User.find_by_id(user_id)
+  end
+
+  def question
+    Question.find_by_question_id(question_id)
+  end
+
+  def parent_reply
+    Reply.find_by_id(parent_reply_id)
+  end
+
+  def child_replies
+    Reply.all.select {|reply| reply.parent_reply_id == @id}
+  end
+end
+
+
+class QuestionLike
+  attr_accessor :id, :user_id, :question_id
+  
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM question_likes")
+    data.map { |datum| QuestionLike.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        question_likes
+      WHERE
+        id = ?
+    SQL
+
+    QuestionLike.new(data.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end
+end
